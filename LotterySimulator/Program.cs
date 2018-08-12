17d8885e587b5c53be72c343e4e90c2ad978330a
nicoleson <mailtonicoles@yahoo.com>
2018-08-12 13:19:31 +1000

    Add files via upload

---
+++ LotterySimulator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LotterySimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lotteryLength = 6;
+            int[] lotteryNumbers = new int[lotteryLength];
+
+            Random random = new Random();
+
+            int lotteryNumberIndex = 0;
+            while (lotteryNumberIndex < lotteryLength)
+            {
+                lotteryNumbers[lotteryNumberIndex] = random.Next(1, 46);
+
+                bool hasDuplicate = false;
+                for (int i = 0; i < lotteryNumberIndex; i++)
+                {
+                    if (lotteryNumbers[lotteryNumberIndex] == lotteryNumbers[i])
+                    {
+                        hasDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (!hasDuplicate)
+                {
+                    lotteryNumberIndex++;
+                }
+            }
+
+            Console.Write("Lottery Number: ");
+            for (int i = 0; i < lotteryLength; i++)
+            {
+                Console.Write(lotteryNumbers[i]);
+                Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
